32e6b2ae9ea335d76e4ef3ae0655584699e53e87
unknown <>
2017-10-16 21:18:37 +0800

    增加首页分类是否显示标志

--- models/classify.js
+++ models/classify.js
@@ -3,11 +3,13 @@ import mongoose from 'mongoose'
 const Schema = mongoose.Schema({
 	name     : String,
 	remark   : String,
+	is_show  : Boolean,
 	create_at: {
 		type   : Date,
 		default: Date.now(),
 	},
 	update_at: Date,
+
 })
 
 export default mongoose.model('classify', Schema)
